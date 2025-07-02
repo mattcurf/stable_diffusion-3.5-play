271b358371c5ebbcee03ab67eb8d4b464859af24
Matt Curfman <matt@tecnovelty.com>
2025-07-02 10:15:03 -0700

    NV support

--- sd3_infer.py
+++ sd3_infer.py
@@ -82,7 +82,7 @@ CLIPG_CONFIG = {
 class ClipG:
     def __init__(self, model_folder: str, device: str = "cpu"):
         with safe_open(
-            f"{model_folder}/clip_g.safetensors", framework="pt", device="cpu"
+            f"{model_folder}/text_encoders/clip_g.safetensors", framework="pt", device="cpu"
         ) as f:
             self.model = SDXLClipG(CLIPG_CONFIG, device=device, dtype=torch.float32)
             load_into(f, self.model.transformer, "", device, torch.float32)
@@ -100,7 +100,7 @@ CLIPL_CONFIG = {
 class ClipL:
     def __init__(self, model_folder: str):
         with safe_open(
-            f"{model_folder}/clip_l.safetensors", framework="pt", device="cpu"
+            f"{model_folder}/text_encoders/clip_l.safetensors", framework="pt", device="cpu"
         ) as f:
             self.model = SDClipModel(
                 layer="hidden",
@@ -126,7 +126,7 @@ T5_CONFIG = {
 class T5XXL:
     def __init__(self, model_folder: str, device: str = "cpu", dtype=torch.float32):
         with safe_open(
-            f"{model_folder}/t5xxl.safetensors", framework="pt", device="cpu"
+            f"{model_folder}/text_encoders/t5xxl_fp16.safetensors", framework="pt", device="cpu"
         ) as f:
             self.model = T5XXLModel(T5_CONFIG, device=device, dtype=dtype)
             load_into(f, self.model.transformer, "", device, dtype)
